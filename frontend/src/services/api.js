99c91a20ec80a04e32c2c86c14c735e5dc655e4b
praveen <praveenmatta84@gmail.com>
2026-07-18 14:32:14 +0530

    ue deployed backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,20 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-})
+});
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
-  return config
-})
+  return config;
+});
 
-export default api
+export default api;
